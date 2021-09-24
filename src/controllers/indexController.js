2f992b8eb6f12e651f350598570f532cc0f61cff
DávalosLeonardo <davaloslm@hotmail.com.ar>
2021-09-24 08:40:18 -0300

    Agrega parametro id a la ruta detalle y código ejs en detalleMenu

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -4,6 +4,12 @@ const controller = {
     index: function(req, res, next) {
         res.render('index', {platos});
       },
+    detalle:  (req, res)=>{
+      const {id} = req.params;
+      const plato = platos.find(plato=>plato.id===parseInt(id))
+      res.render("detalleMenu", {plato});
+      
+  }
 
 }
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { index } = require("../controllers/indexController")
+const { index, detalle } = require("../controllers/indexController")
 
 
 /* GET home page. */
 router.get('/', index);
+router.get('/detalle/:id', detalle);
 
 module.exports = router;
